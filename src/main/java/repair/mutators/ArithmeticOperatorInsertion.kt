0112029064907481d1fa14196ee5fc3f8d2cb703
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-07-31 11:38:04 +0100

    Change COI to accept Expression instead of just BinaryExpr; Update mutators in RepairStrategy to use some default values for COI when no landmark info is present; Implement ArithmeticOperatorInsertion mutation operator

---
+++ src/main/java/repair/mutators/ArithmeticOperatorInsertion.kt
@@ -0,0 +1,44 @@
+package repair.mutators
+
+import BuggyProgram
+import com.github.javaparser.ast.expr.*
+import com.github.javaparser.ast.expr.BinaryExpr.Operator.*
+import repair.mutators.utils.calcType
+import repair.mutators.utils.isArithmetic
+import java.lang.NumberFormatException
+
+class ArithmeticOperatorInsertion(val op: BinaryExpr.Operator? = null, val exprToAdd: Expression? = null): MutatorRepair<Expression>() {
+    override val rank: Int
+        get() = 18
+
+    override fun checkedRepair(program: BuggyProgram, expr: Expression): List<BinaryExpr> {
+        return if(op == null && exprToAdd == null){
+            val exprType = calcType(expr) ?: return emptyList()
+            when(exprType.describe()){
+                "int" -> return listOf(Pair(1, PLUS), Pair(1, MINUS),
+                        Pair(Int.MAX_VALUE, PLUS), Pair(Int.MAX_VALUE, MINUS), Pair(Int.MAX_VALUE, MULTIPLY),
+                        Pair(Int.MAX_VALUE, DIVIDE), Pair(Int.MIN_VALUE, PLUS), Pair(Int.MIN_VALUE, MINUS),
+                        Pair(Int.MIN_VALUE, MULTIPLY), Pair(Int.MIN_VALUE, DIVIDE)
+                ).map { BinaryExpr(expr.clone(), IntegerLiteralExpr(it.first.toString()), it.second) }
+                "long" -> return listOf(Pair(1, PLUS), Pair(1, MINUS),
+                        Pair(Long.MAX_VALUE, PLUS), Pair(Long.MAX_VALUE, MINUS), Pair(Long.MAX_VALUE, MULTIPLY),
+                        Pair(Long.MAX_VALUE, DIVIDE), Pair(Long.MIN_VALUE, PLUS), Pair(Long.MIN_VALUE, MINUS),
+                        Pair(Long.MIN_VALUE, MULTIPLY), Pair(Long.MIN_VALUE, DIVIDE)
+                ).map { BinaryExpr(expr.clone(), LongLiteralExpr(it.first.toString()), it.second) }
+                "double" -> return listOf(Pair(1, PLUS), Pair(1, MINUS),
+                        Pair(Double.MAX_VALUE, PLUS), Pair(Double.MAX_VALUE, MINUS), Pair(Double.MAX_VALUE, MULTIPLY),
+                        Pair(Double.MAX_VALUE, DIVIDE), Pair(Double.MIN_VALUE, PLUS), Pair(Double.MIN_VALUE, MINUS),
+                        Pair(Double.MIN_VALUE, MULTIPLY), Pair(Double.MIN_VALUE, DIVIDE)
+                ).map { BinaryExpr(expr.clone(), DoubleLiteralExpr(it.first.toString()), it.second) }
+                "float" -> return listOf(Pair(1, PLUS), Pair(1, MINUS),
+                        Pair(Double.MAX_VALUE, PLUS), Pair(Double.MAX_VALUE, MINUS), Pair(Double.MAX_VALUE, MULTIPLY),
+                        Pair(Double.MAX_VALUE, DIVIDE), Pair(Double.MIN_VALUE, PLUS), Pair(Double.MIN_VALUE, MINUS),
+                        Pair(Double.MIN_VALUE, MULTIPLY), Pair(Double.MIN_VALUE, DIVIDE)
+                ).map { BinaryExpr(expr.clone(), DoubleLiteralExpr(it.first.toString()), it.second) }
+                else -> emptyList()
+            }
+//        } else if (isArithmetic(op)){
+
+        } else emptyList()
+    }
+}
--- src/main/java/repair/mutators/ConditionalOperatorInsertion.kt
+++ src/main/java/repair/mutators/ConditionalOperatorInsertion.kt
@@ -2,11 +2,12 @@ package repair.mutators
 
 import BuggyProgram
 import com.github.javaparser.ast.expr.BinaryExpr
+import com.github.javaparser.ast.expr.Expression
 import repair.mutators.utils.isConditional
 
-class ConditionalOperatorInsertion(val op: BinaryExpr.Operator, val exprToAdd: BinaryExpr): MutatorRepair<BinaryExpr>() {
+class ConditionalOperatorInsertion(val op: BinaryExpr.Operator, val exprToAdd: Expression): MutatorRepair<BinaryExpr>() {
     override val rank: Int
-        get() = 22
+        get() = 17
 
     override fun checkedRepair(program: BuggyProgram, binExpr: BinaryExpr): List<BinaryExpr> {
         return if (isConditional(op)){
