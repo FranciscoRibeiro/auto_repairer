6227666d1bfe012c1d20a13cf0a32bb5971bd0d8
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-11-20 11:35:52 +0000

    Fix ArithmeticOperatorDeletion to generate only one mutant when both sides of the expression are equal

--- src/main/java/repair/mutators/ArithmeticOperatorDeletion.kt
+++ src/main/java/repair/mutators/ArithmeticOperatorDeletion.kt
@@ -13,6 +13,7 @@ class ArithmeticOperatorDeletion: MutatorRepair<BinaryExpr>() {
     override fun checkedRepair(program: BuggyProgram, binExpr: BinaryExpr): List<Expression> {
         return if(isArithmetic(binExpr.operator)){
             return if(isString(binExpr.left) || isString(binExpr.right)) emptyList()
+            else if (binExpr.right == binExpr.left) listOf(binExpr.left.clone()) // Both sides are the same, produce only one
             else listOf(binExpr.left.clone(), binExpr.right.clone())
         } else { emptyList() }
     }
