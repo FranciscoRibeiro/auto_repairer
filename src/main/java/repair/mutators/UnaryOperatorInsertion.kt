010d9550edad3e233e26f0423b8012c5c032b552
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-11-20 11:32:34 +0000

    Change UnaryOperatorInsertion to not add the MINUS operator (already covered by Negation) and to also enclose expressions between parenthesis (associativity issues)

--- src/main/java/repair/mutators/UnaryOperatorInsertion.kt
+++ src/main/java/repair/mutators/UnaryOperatorInsertion.kt
@@ -1,10 +1,7 @@
 package repair.mutators
 
 import BuggyProgram
-import com.github.javaparser.ast.expr.BinaryExpr
-import com.github.javaparser.ast.expr.Expression
-import com.github.javaparser.ast.expr.NameExpr
-import com.github.javaparser.ast.expr.UnaryExpr
+import com.github.javaparser.ast.expr.*
 import com.github.javaparser.resolution.types.ResolvedPrimitiveType
 import repair.mutators.utils.calcType
 import repair.mutators.utils.getParent
@@ -17,7 +14,8 @@ class UnaryOperatorInsertion(): MutatorRepair<Expression>() {
 
     private val exclusions = listOf(UnaryExpr.Operator.LOGICAL_COMPLEMENT,
             UnaryExpr.Operator.BITWISE_COMPLEMENT,
-            UnaryExpr.Operator.PLUS)
+            UnaryExpr.Operator.PLUS,
+            UnaryExpr.Operator.MINUS) // The Negation mutation operator covers MINUS
 
     override fun checkedRepair(program: BuggyProgram, expr: Expression): List<Expression> {
         if(isLeftSideAssign(expr)) return emptyList()
@@ -26,24 +24,23 @@ class UnaryOperatorInsertion(): MutatorRepair<Expression>() {
         if(parent is UnaryExpr) return emptyList()
 
         return if(expr is NameExpr){
-//            val type = expr.calculateResolvedType()
             val type = calcType(expr) ?: return emptyList()
             if(type is ResolvedPrimitiveType){
                 when {
                     type.isNumeric -> {
                         UnaryExpr.Operator.values()
                                 .subtract(exclusions)
-                                .map { UnaryExpr(expr.clone(), it) }
+                                .map { UnaryExpr(EnclosedExpr(expr.clone()), it) }
                     }
-                    type.name == "BOOLEAN" -> listOf(UnaryExpr(expr.clone(), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
+                    type.name == "BOOLEAN" -> listOf(UnaryExpr(EnclosedExpr(expr.clone()), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
                     else -> emptyList()
                 }
             } else emptyList()
 
         } else if(expr is BinaryExpr && isConditional(expr.operator)){
-            listOf(UnaryExpr(expr.clone(), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
+            listOf(UnaryExpr(EnclosedExpr(expr.clone()), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
         } else if(parent is BinaryExpr && isConditional(parent.operator)){
-            listOf(UnaryExpr(expr.clone(), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
+            listOf(UnaryExpr(EnclosedExpr(expr.clone()), UnaryExpr.Operator.LOGICAL_COMPLEMENT))
         } else emptyList()
     }
 }
