21bfb298d103840263a406f81504e8e4443f2f24
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-11-23 10:35:41 +0000

    Fix all_diff.sh utility script

--- util_scripts/all_diff.sh
+++ util_scripts/all_diff.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 DIR="$1"
-FILES=($(ls "$DIR"))
+FILES=($(find "$DIR" -name "*java"))
 
 for f in ${!FILES[@]}
 do
 	next_idx=$((f+1))
 	for ((f1=$next_idx;f1<${#FILES[@]};f1++))
 	do
-		diff $DIR/${FILES[$f]} $DIR/${FILES[$f1]} &> /dev/null && echo "$DIR/${FILES[$f]} and $DIR/${FILES[$f1]} are identical"
+		diff ${FILES[$f]} ${FILES[$f1]} &> /dev/null && echo "${FILES[$f]} and ${FILES[$f1]} are identical"
 	done
 done
 
