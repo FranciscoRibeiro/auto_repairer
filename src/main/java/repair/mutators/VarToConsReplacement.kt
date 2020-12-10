5132f545608cf75801b2d28278ef78c209822a1b
FranciscoRibeiro <kikoribeiro95@gmail.com>
2020-12-10 16:01:17 +0000

    Make VarToConsReplacement use -1, 0 and 1 as default values

--- src/main/java/repair/mutators/VarToConsReplacement.kt
+++ src/main/java/repair/mutators/VarToConsReplacement.kt
@@ -1,16 +1,14 @@
 package repair.mutators
 
 import BuggyProgram
-import com.github.javaparser.ast.Node
-import com.github.javaparser.ast.body.Parameter
 import com.github.javaparser.ast.expr.*
-import com.github.javaparser.resolution.UnsolvedSymbolException
-import printError
 import repair.mutators.utils.*
 
 class VarToConsReplacement: MutatorRepair<NameExpr>() {
     override val rank: Int
         get() = 19
+
+    val defaultValues = listOf("1", "0", "-1").map { IntegerLiteralExpr(it) }
 
     override fun checkedRepair(program: BuggyProgram, nameExpr: NameExpr): List<LiteralStringValueExpr> {
         val parent = getParent(nameExpr)
@@ -21,26 +19,18 @@ class VarToConsReplacement: MutatorRepair<NameExpr>() {
             }
         }
 
-        val methodDecl = getEnclosing(nameExpr) ?: return emptyList()
-//        val type = nameExpr.calculateResolvedType()
-        val type = calcType(nameExpr) ?: return emptyList()
-        if(type.isArray) return emptyList()
+        val enclosingDecl = getEnclosing(nameExpr) ?: return emptyList()
+        val type = calcType(nameExpr)
+        //if it is an array or NOT a number
+        if(type != null && (type.isArray || !isTypeNumber(type))) return emptyList()
 
-        return if(isTypeNumber(type)){
-            val constants = methodDecl.findAll(LiteralStringValueExpr::class.java, { isNumeric(it) })
-                                    .distinctBy { it.value }
-                                    .map { it.clone() }
+        val constants = (enclosingDecl.findAll(LiteralStringValueExpr::class.java, { isNumeric(it) }) + defaultValues)
+                                .distinctBy { it.value }
+                                .map { it.clone() }
 
-//            val operation = nameExpr.findAncestor(BinaryExpr::class.java).orElse(null)
-            return if(parent is BinaryExpr && isArithmetic(parent.operator)){
-                constants.filter { !invalidOperation(parent, nameExpr, it) }
-            } else constants
-//            return if(operation == null || !isArithmetic(operation.operator)) {
-//                constants
-//            } else {
-//                constants.filter { !invalidOperation(operation, nameExpr, it) }
-//            }
-        } else emptyList()
+        return if(parent is BinaryExpr && isArithmetic(parent.operator)){
+            constants.filter { !invalidOperation(parent, nameExpr, it) }
+        } else constants
     }
 
     private fun invalidOperation(expr: BinaryExpr, variable: NameExpr, cons: LiteralStringValueExpr): Boolean {
